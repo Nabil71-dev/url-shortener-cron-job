6828d51002a7040bab1072f73321b26a87c40714
Nabil71-dev <mahmudnabiliiuc@gmail.com>
2023-11-15 20:21:19 +0600

    URL changed

--- netlify/functions/deploy.js
+++ netlify/functions/deploy.js
@@ -1,12 +1,12 @@
-const { schedule } =require("@netlify/functions")
+const { schedule } = require("@netlify/functions")
 const axios = require('axios');
 
-const handler = async function(event, context) {
-        try {
-        const response = axios.get(`https://url-shortner-nabil-be.vercel.app/scheduler/cron-job`);
-        console.log("Successfully done",response)
+const handler = async function (event, context) {
+    try {
+        const response = axios.get(`https://shortyurl-server.vercel.app/scheduler/cron-job`);
+        console.log("Successfully done", response)
     } catch (error) {
-        console.log("Have some error on it",error)
+        console.log("Have some error on it", error)
     }
 };
 
